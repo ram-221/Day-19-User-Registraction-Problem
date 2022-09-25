399971904b5af4a077b2a0906ce0ad3ef7e7f567
ram-221 <rameshmalli857.com>
2022-09-25 20:44:45 +0530

    Rule-1,2 and Should have at least 1 numeric number in the password

---
+++ UserRegistractionUC7.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+import java.util.Scanner;
+
+public class UserRegistractionUC7 {
+
+	public static void main(String[] args) {
+		// get user input
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter Passord:");
+		String password = scanner.nextLine();
+
+		// validate user input and display error message
+		System.out.println("\nValidate Result:");
+		if(!validatePassword(password))
+			System.out.println("Invalid Password");			
+		else
+			System.out.println("Valid Input. Thank you.");
+	}
+
+//	 validate rule 3 password
+	
+	private static boolean validatePassword(String password) {
+		return password.matches("[A-Z]{1}[a-z]{8}[0-9]{1}");
+	}
+
+}
